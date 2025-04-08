76f279925ea8b488da200f4d194a92656517db14
pizzaman1003 <197116207+pizzman1003@users.noreply.github.com>
2025-04-08 11:45:23 -0400

    first commit

---
+++ devProject/Calculator/Program.cs
@@ -0,0 +1,75 @@
+﻿//Calculator
+//4 functions: + - * / = 4 switch cases for x and y
+Console.WriteLine("To Use:");
+Console.WriteLine("-Entry of Operands");
+Console.WriteLine("-Selection of Operator");
+Console.WriteLine("Press Enter to Continue");
+Console.ReadLine();
+
+Console.WriteLine("Enter your first number:");
+decimal num1 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Enter your second number:");
+decimal num2 = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("Select operation to be performed:");
+Console.WriteLine("-Type 1 for Addition");
+Console.WriteLine("-Type 2 for Subtraction");
+Console.WriteLine("-Type 3 for Multiplication");
+Console.WriteLine("-Type 4 for Division");
+
+int choice = Convert.ToInt32(Console.ReadLine());
+decimal result = 0;
+switch (choice)
+{
+    case 1:
+    result = Addition(num1, num2);
+    break;
+
+    case 2:
+    result = Subtraction(num1, num2);
+    break;
+
+    case 3:
+    result = Multiplication(num1, num2);
+    break;
+
+    case 4:
+    result = Division(num1, num2);
+    break;
+
+    default:
+    Console.WriteLine("You did not select an operation");
+    break;
+}
+
+Console.WriteLine($"The result is: {result}");
+
+decimal Addition(decimal num1, decimal num2)
+{
+    result = num1 + num2;
+    return result;
+}
+decimal Subtraction(decimal num1, decimal num2)
+{
+    result = num1 - num2;
+    return result;
+}
+decimal Multiplication(decimal num1, decimal num2)
+{
+    result = num1 * num2;
+    return result;
+}
+decimal Division(decimal num1, decimal num2)
+{
+    if (num2 != 0)
+    {
+        result = num1 / num2;
+        return result;
+    }
+    else
+    {
+        Console.WriteLine("You cannot divide by zero");
+    }
+    return 0;
+}
---
+++ devProject/Program.cs
@@ -0,0 +1,38 @@
+﻿//Math game. Practice addition, subtraction, multiplication, and division.
+/* TODO
+1. Interface: Welcome screen, operator selector, win/loss
+2. Functions for operators.
+3. Difficulties? Instead of 2 numbers have multiple or additional operators.
+*/
+namespace MathGame
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int winNum = random.Next(0, 100);
+            bool win = false;
+            
+            do
+            {
+                Console.WriteLine("Guess my hidden number, 0-100");
+                string guess = Console.ReadLine();
+                int numberGuess = int.Parse(guess);
+
+                if(numberGuess > winNum)
+                {
+                    Console.WriteLine($"{numberGuess} is too high, try lower...");
+                }
+                else if (numberGuess < winNum)
+                {
+                    Console.WriteLine($"{numberGuess} is too low, try higher...");
+                }
+                else if (numberGuess == winNum)
+                {
+                    Console.WriteLine($"You did it! My number was {winNum}");
+                }
+            } while (win == false);
+        }
+    }
+}
